96ebd9cec303593fa542eaf0d3934aa4fbf49770
desa-thomas <desa2thomas@gmail.com>
2024-06-13 23:19:20 -0400

    modified:   index.js 	modified:   package-lock.json 	modified:   package.json 	modified:   views/index.html

--- index.js
+++ index.js
@@ -4,6 +4,9 @@
 // init project
 var express = require('express');
 var app = express();
+let bodyParser = require("body-parser")
+
+app.use(bodyParser.urlencoded({extended: true}))
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
@@ -24,9 +27,39 @@ app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+// POST request handler for form submission. 
+app.route("/api").post(function(req, res){
+  console.log(req.body.date)
+  res.redirect('/api/' + req.body.date)
+})
+
+
+app.get("/api/:date", 
+  //First function converts the param into a date object
+  function(req, res, next){
+    //Check if its NaN
+    if(isNaN(req.params.date)){
+      req.date = new Date(req.params.date)
+    }
+    //If its a number (unix), type cast it
+    else{
+      req.date = new Date(Number(req.params.date))
+    }
+    next()
+}, 
+  function(req, res){
+    
+    //date is valid 
+    if(!isNaN(req.date)){
+      res.json({unix: req.date.getTime(), utc: req.date.toUTCString()})
+    }
+    else{
+      res.json({error: "Invalid Date"})
+    }   
+})
 
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
+  console.log('Your app is listening on port ' + "http://localhost:"+listener.address().port);
 });
